d8d89361efc564136f90d3081817f4e680c5df66
HORIE Tetsuya <horie-t@users.noreply.github.com>
2024-05-05 14:12:34 +0900

    OMS-29 メッセージを1秒おきに出力する

--- l_chika/l_chika/l_chika_node.py
+++ l_chika/l_chika/l_chika_node.py
@@ -5,12 +5,16 @@ class LChikaNode(Node):
     def __init__(self):
         print('Generate Node')
         super().__init__('l_chika_node')
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
         self.get_logger().info('Hi from l_chika.')
 
 def main():
     print('Start program')
     rclpy.init()
     node = LChikaNode()
+    rclpy.spin(node)
     rclpy.shutdown
     print('Finish program')
 

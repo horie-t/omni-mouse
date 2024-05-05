1ae8ab8ba1a409f94a1a5212fe02e63b33b2ad7b
HORIE Tetsuya <horie-t@users.noreply.github.com>
2024-05-05 15:01:41 +0900

    OMS-29 Lチカを動くようにした

--- l_chika/l_chika/l_chika_node.py
+++ l_chika/l_chika/l_chika_node.py
@@ -1,14 +1,18 @@
 import rclpy
 from rclpy.node import Node
+from gpiozero import LED
 
 class LChikaNode(Node):
     def __init__(self):
         print('Generate Node')
         super().__init__('l_chika_node')
-        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.led = LED(25)
+        self.led.on()
+        self.timer = self.create_timer(0.5, self.timer_callback)
 
     def timer_callback(self):
         self.get_logger().info('Hi from l_chika.')
+        self.led.toggle()
 
 def main():
     print('Start program')

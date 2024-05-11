d89589eb443dd8de546f82caeaac15b04c91de57
HORIE Tetsuya <horie-t@users.noreply.github.com>
2024-05-11 10:17:57 +0900

    OMS-16 MCP3008のADコンバーターでJoy Stickの入力を読み込む

---
+++ read_adc/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'read_adc'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tetsuya',
+    maintainer_email='horie-t@users.noreply.github.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'read_adc_node = read_adc.read_adc_node:main'
+        ],
+    },
+)

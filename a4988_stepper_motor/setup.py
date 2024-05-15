881a181b8a01320b5bb9ec0b51852f7a73beceb8
HORIE Tetsuya <horie-t@users.noreply.github.com>
2024-05-15 18:01:28 +0900

    OMS-30 A4988ステッピングモーターを接続

---
+++ a4988_stepper_motor/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'a4988_stepper_motor'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tetsuya',
+    maintainer_email='horie-t@users.noreply.github.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'a4988_stepper_motor_node = a4988_stepper_motor.a4988_stepper_motor_node:main'
+        ],
+    },
+)

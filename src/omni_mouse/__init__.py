13d8b18e83f9d814fbec22b34c7f61a5577e9e33
HORIE Tetsuya <horie-t@users.noreply.github.com>
2025-01-29 08:38:27 +0900

    #36 台車制御Actorを実装する
    固定された動きをするだけ

--- src/omni_mouse/__init__.py
+++ src/omni_mouse/__init__.py
@@ -1,2 +1,11 @@
+import time
+
+from omni_mouse.model import Twist, Vector3
+from omni_mouse.motion_control import MotionControllActor
+
 def main() -> None:
-    print("Hello from omni-mouse!")
+    velocity = Twist(linear=Vector3(1, 0, 0), angular=Vector3(0, 0, 0))
+    actor = MotionControllActor.remote()
+    actor.run.remote(velocity)
+    time.sleep(10)
+    actor.stop.remote()
---
+++ src/omni_mouse/motion_control.py
@@ -0,0 +1,47 @@
+from math import cos, pi, sin
+import numpy as np
+import ray
+from stspin import (
+    SpinChain,
+    SpinDevice,
+    Constant as StConstant,
+    Register as StRegister,
+    utility,
+)
+
+from omni_mouse.model import Twist
+
+@ray.remote
+class MotionControllActor:
+    def __init__(self, wheel_radius: float = 0.024, shaft_length: float = 0.05):
+        self.wheel_radius = wheel_radius
+        self.shaft_length = shaft_length
+        self.rotate_mat = (1 / wheel_radius) * np.array([
+            [cos(  pi      - pi / 2), sin(  pi      - pi / 2), - shaft_length],
+            [cos(  pi / 3  - pi / 2), sin(  pi / 3  - pi / 2), - shaft_length],
+            [cos(-(pi / 3) - pi / 2), sin(-(pi / 3) - pi / 2), - shaft_length]
+        ])
+
+        self.motors = []
+        for i in range(3):
+            st_chain = SpinChain(total_devices=1, spi_select=(1, i))
+            motor = st_chain.create(0)
+            self.motors.append(motor)
+
+    def run(self, velocity: Twist):
+        speeds = self._calc_speed_of_wheels(np.array([velocity.linear.x, velocity.linear.y, velocity.angular.z * 20]))
+        print(speeds)
+        for speed, motor in zip(speeds, self.motors):
+            if speed >= 0:
+                motor.setDirection(StConstant.DirReverse)
+                motor.run(speed)
+            else:
+                motor.setDirection(StConstant.DirForward)
+                motor.run(-speed)
+
+    def stop(self):
+        for motor in self.motors:
+            motor.hiZHard()
+
+    def _calc_speed_of_wheels(self, vec):
+        return np.dot(self.rotate_mat, vec)

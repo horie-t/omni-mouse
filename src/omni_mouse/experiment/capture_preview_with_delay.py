7bec9cbf95b293b26ff04496b27715da45d19480
HORIE Tetsuya <horie-t@users.noreply.github.com>
2025-04-13 12:47:38 +0900

    #67 校正用画像の取得を終了するキーをqに変更する

--- src/omni_mouse/experiment/capture_preview_with_delay.py
+++ src/omni_mouse/experiment/capture_preview_with_delay.py
@@ -7,7 +7,7 @@ tuning = Picamera2.load_tuning_file("pi5_imx219_160d.json")
 picam2 = Picamera2(tuning=tuning)
 picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (800, 600)}))
 # picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (3280, 2464)}))
-capture_config = picam2.create_still_configuration()
+capture_config = picam2.create_still_configuration(main={"size": (1640, 1232)})
 
 picam2.start_preview(Preview.QTGL)
 picam2.start()
@@ -16,7 +16,7 @@ def capture_image():
     """3秒後に画像をキャプチャする関数"""
     time.sleep(3)
     timestamp = time.strftime("%Y%m%d-%H%M%S")
-    picam2.switch_mode_and_capture_file(capture_config, f"image_{timestamp}.jpg")
+    picam2.switch_mode_and_capture_file(capture_config, f"image_{timestamp}.png")
     print(f"画像が image_{timestamp}.jpg として保存されました。")
 
 def on_press(key):
@@ -26,6 +26,9 @@ def on_press(key):
             print("yキーが押されました。3秒後に画像をキャプチャします。")
             # 新しいスレッドで画像キャプチャ関数を実行
             threading.Thread(target=capture_image).start()
+        elif key.char == 'n':
+            print("nキーが押されました。プログラムを終了します。")
+            return False
     except AttributeError:
         # 特殊なキー（Shift、Ctrlなど）は無視
         pass

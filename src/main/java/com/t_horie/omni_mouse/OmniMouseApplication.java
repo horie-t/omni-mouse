765b8a3fd1c8ff1617d15f1970ada1edef1ebfe6
HORIE Tetsuya <horie-t@users.noreply.github.com>
2026-03-21 11:57:33 +0900

    定期的に標準出力にHello, world!を出力する

--- src/main/java/com/t_horie/omni_mouse/OmniMouseApplication.java
+++ src/main/java/com/t_horie/omni_mouse/OmniMouseApplication.java
@@ -1,7 +1,12 @@
 package com.t_horie.omni_mouse;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class OmniMouseApplication {
@@ -10,4 +15,10 @@ public class OmniMouseApplication {
 		SpringApplication.run(OmniMouseApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run() {
+		return args -> Flux.interval(Duration.ofSeconds(1))
+				.map(i -> "Hello, world! (" + i + ")")
+				.subscribe(IO::println);
+	}
 }
--- src/test/java/com/t_horie/omni_mouse/OmniMouseApplicationTests.java
+++ src/test/java/com/t_horie/omni_mouse/OmniMouseApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class OmniMouseApplicationTests {
 
 	@Test
 	void contextLoads() {

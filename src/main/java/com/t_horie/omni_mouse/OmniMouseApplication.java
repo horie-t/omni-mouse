97772697acd45c1cb2069ceef4facb40d651ca8d
HORIE Tetsuya <horie-t@users.noreply.github.com>
2026-03-21 11:44:10 +0900

    Spring WebFluxに変更する

---
+++ src/main/java/com/t_horie/omni_mouse/OmniMouseApplication.java
@@ -0,0 +1,13 @@
+package com.t_horie.omni_mouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniMouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniMouseApplication.class, args);
+	}
+
+}
